b2751b32d1377adeaf4aaa99b665ace2d9d0fc00
23034361 <23034361@myrp.edu.sg>
2026-01-06 10:33:23 +0800

    commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // include the required packages
 const express = require('express' );
-const mysql = require('mysq12/promise');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 const port = 3000;
 
